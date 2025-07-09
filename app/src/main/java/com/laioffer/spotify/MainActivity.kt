adbbb52aa71e9e077b894d8f24d8d087d5398417
Andy Zhang <zhangnd16@berkeley.edu>
2025-07-08 18:43:14 -0700

    Fragment, navigation bar set up

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -1,80 +1,112 @@
 package com.laioffer.spotify
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import coil.compose.AsyncImage
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.laioffer.spotify.ui.theme.SpotifyTheme
 
-// customized extend AppCompatActivity
 class MainActivity : AppCompatActivity() {
-    private val TAG = "lifecycle"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "We are at onCreate()")
-        setContent {
-            SpotifyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+//        setContent {
+//            SpotifyTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface {
+//                    Greeting("Android")
+//                }
+//
+//            }
+//        }
+        setContentView(R.layout.activity_main)
+
+        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
+
+        val navHostFragment =supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        navController.setGraph(R.navigation.nav_graph)
+
+        NavigationUI.setupWithNavController(navView, navController)
+
+        navView.setOnItemSelectedListener{
+            NavigationUI.onNavDestinationSelected(it, navController)
+            navController.popBackStack(it.itemId, inclusive = false)
+            true
         }
     }
 }
 
+
 @Composable
-fun ArtistCardBox() {
-    Box {
-        Text("Alfred Sisley")
-        Text("3 minutes ago")
+private fun LoadingSection(text: String) {
+    Row(modifier = Modifier.padding(vertical = 8.dp)) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.body2,
+            color = Color.White
+        )
     }
 }
 
 @Composable
-fun ArtistCardColumn() {
+private fun AlbumCover() {
     Column {
-        Text("Alfred Sisley")
-        Text("3 minutes ago")
-    }
-}
+        Box(modifier = Modifier.size(160.dp)) {
+            AsyncImage(
+                model = "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg",
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds,
+            )
 
-@Composable
-fun ArtistCardRow() {
-    Row {
-        Text("Alfred Sisley")
-        Text("3 minutes ago")
-    }
-}
-
-@Preview
-@Composable
-fun PreviewArtistCardRow() {
-    SpotifyTheme {
-        Surface {
-            ArtistCardRow()
+            Text(
+                text = "Still Fantasy",
+                color = Color.White,
+                modifier = Modifier
+                    .padding(bottom = 4.dp, start = 2.dp)
+                    .align(Alignment.BottomStart),
+            )
         }
+
+        Text(
+            text = "Jay Chou",
+            modifier = Modifier.padding(top = 4.dp),
+            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
+            color = Color.LightGray,
+        )
     }
 }
 
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", color = Color.White)
 }
 
 @Preview(showBackground = true)
@@ -82,7 +114,7 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     SpotifyTheme {
         Surface {
-            Greeting("Android")
+            AlbumCover()
         }
     }
 }
